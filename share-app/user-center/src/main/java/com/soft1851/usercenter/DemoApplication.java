b1245ffeb92796b73d6fe062fdee75adaaefb2d0
zhao-rgb <1019916061@qq.com>
2020-09-22 21:52:51 +0800

    spring cloud

---
+++ share-app/user-center/src/main/java/com/soft1851/usercenter/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.soft1851.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
